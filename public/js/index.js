a6181e28ce3101357d7ebe8173bb8abd182fd892
saadtanveer427 <saadtanveer427@gmail.com>
2022-11-06 21:43:04 +0500

    completedBookingalerts

--- public/js/index.js
+++ public/js/index.js
@@ -384,3 +384,7 @@ if (bookBtn)
     const { tourId } = e.target.dataset;
     bookTour(tourId);
   });
+
+
+  const alertMessage=document.querySelector('body').dataset.alert;
+  if(alertMessage) showAlert('success',alertMessage)

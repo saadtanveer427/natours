9ea50198bbbf2167432ec21ccec5468f5b68259a
saadtanveer427 <saadtanveer427@gmail.com>
2022-11-07 16:25:46 +0500

    changes_disablebuttonbooking

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -28,6 +28,7 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 
 exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (including reviews and guides)
+
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user'
@@ -39,10 +40,17 @@ exports.getTour = catchAsync(async (req, res, next) => {
 
   // 2) Build template
   // 3) Render template using data from 1)
+
+  
+if(await Booking.findOne({user:req.user._id,tour:tour._id,paid:true})){
+  res.locals.booked=true;
+}
+
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour
   });
+  
 });
 
 exports.getLoginForm = (req, res) => {

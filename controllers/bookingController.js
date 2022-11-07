a627ca262af59f01e2323d71ee98086769045a67
saadtanveer427 <saadtanveer427@gmail.com>
2022-11-07 10:12:54 +0500

    updated_latest_app

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -50,7 +50,7 @@ const createBookingCheckout = async session =>{
   const tour = session.client_reference_id
   const user= (await User.findOne({email:session.customer_details.email}))._id
  const price = session.amount_total / 100;
-  console.log(await Booking.create({ tour, user, price }));
+  await Booking.create({ tour, user, price });
 
 }
 
